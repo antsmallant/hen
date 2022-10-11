f00a67dd399e2496b4e16cf23bb4cf124ad5bbcf
antsmallant <412288482@qq.com>
2022-10-11 17:15:14 +0800

    add luacheck

---
+++ tools/install_luacheck.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo luarocks install luacheck
---
+++ tools/luacheck.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+flag="--no-unused --no-unused-args --no-unused-secondaries 
+    --no-redefined --no-color 
+    --globals class interface table string implements SERVICE_NAME 
+    --ignore 542 512 --no-max-code-line-length"
+
+files=`find ../src/lualib/ -name '*.lua'`
+luacheck $files $flag > luacheck_result.log
+
+files=`find ../src/service/ -name '*.lua'`
+luacheck $files $flag >> luacheck_result.log
+
+#sed -i '/Total: /'d luacheck_result.log
+
+grep -v "Checking.*OK" luacheck_result.log
+
+#cat luacheck_result.log
