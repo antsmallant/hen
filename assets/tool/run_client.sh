b20c7c5580cc7f059b512716302460afa624a796
antsmallant <412288482@qq.com>
2022-10-18 11:47:57 +0800

    run client with local lua

--- assets/tool/run_client.sh
+++ assets/tool/run_client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 curdir=$(pwd)
-cd ../../src/3rd/skynet && lua $curdir/../../src/test/gw_client.lua
+cd ../../src/3rd/skynet && chmod +x ./3rd/lua/lua && ./3rd/lua/lua $curdir/../../src/test/gw_client.lua
